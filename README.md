7fdceb18474369d632c3f48933ebbfaa2da83bbd
하예성 <yeseong9769@daum.net>
2023-11-26 05:00:56 +0900

    Little Changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Proxy_Extension
 Proxy Extension Using PAC(Proxy Auto-Configuration)
+
+## Proxy Server Installation
+https://www.digitalocean.com/community/tutorials/how-to-set-up-squid-proxy-for-private-connections-on-debian-11
--- background.js
+++ background.js
@@ -1,18 +1,15 @@
 var config = {
-    mode: "pac_script",
-    pacScript: {
-      data: "function FindProxyForURL(url, host) {\n" +
-            "  if ((dnsDomainIs(host, '.twitch.tv')) || (dnsDomainIs(host, '.ttvnw.net')))\n" +
-            "    return 'PROXY 52.196.27.154:3128';\n" +
-            "  return 'DIRECT';\n" +
-            "}"
-    }
-  };
-  
-  chrome.proxy.settings.set(
-    {value: config, scope: 'regular'},
-    function() {
-        console.log("설정 적용")
-    }
-  );
-  
+  mode: "pac_script",
+  pacScript: {
+    data: "function FindProxyForURL(url, host) {\n" +
+          "  if ((dnsDomainIs(host, '.twitch.tv')) || (dnsDomainIs(host, '.ttvnw.net')))\n" +
+          "    return 'PROXY 52.196.27.154:3128';\n" +
+          "  return 'DIRECT';\n" +
+          "}"
+  }
+};
+
+chrome.proxy.settings.set(
+  { value: config, scope: 'regular' },
+  function () { }
+);
